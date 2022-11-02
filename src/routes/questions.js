a028ffb04c5a0b1375e3d5ed1dfa7ef736087a95
Kvothe853 <smirnovasairidas@gmail.com>
2022-11-02 17:37:39 +0200

    updated sorting queries

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -2,50 +2,52 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql2/promise");
 
-const { isLoggedIn, isAuthenticated } = require("../middleware");
+const { isLoggedIn } = require("../middleware");
 const { dbConfig } = require("../config");
 
 // get all questions
-// router.get("/", async (req, res) => {
-//   try {
-//     const con = await mysql.createConnection(dbConfig);
-//     const [data] = await con.execute(`
-//     SELECT *
-//     FROM questions
-//     LEFT JOIN comments
-//     ON questions.id = comments.question_id
-//     ORDER BY questions.id ASC
-//     `);
-//     res.send(data);
-//     await con.end();
-//   } catch (err) {
-//     res.status(400).send({ err: "Error Get" });
-//   }
-// });
+// main
 
-router.get("/", async (req, res) => {
-  try {
-    const con = await mysql.createConnection(dbConfig);
-    const [data] = await con.execute(`
-    SELECT *
+// SELECT *
+// FROM questions
+// WHERE questions.archived = 0
+// ORDER BY questions.date ${order}
+
+router.get("/:sort?", async (req, res) => {
+  let order = "DESC";
+  let query = ``;
+
+  if (req.params.sort === "DESC" || req.params.sort === "ASC") {
+    order = req.params.sort;
+    query = `
+    SELECT questions.*, (SELECT COUNT(*) FROM comments c WHERE c.question_id = questions.id AND c.archived = 0) AS total_question_comments
     FROM questions
     WHERE questions.archived = 0
-    ORDER BY questions.date DESC
-    `);
-    res.send(data);
-    await con.end();
-  } catch (err) {
-    res.status(400).send({ err: "Error Get" });
+    ORDER BY questions.date ${order}
+    `;
+  } else if (req.params.sort === "MOST") {
+    order = "DESC";
+    query = `
+    SELECT questions.*, (SELECT COUNT(*) FROM comments c WHERE c.question_id = questions.id AND c.archived = 0) AS total_question_comments
+    FROM questions
+    WHERE questions.archived = 0
+    ORDER BY total_question_comments ${order}
+    
+    `;
+  } else if (req.params.sort === "LEAST") {
+    order = "ASC";
+    query = `
+    SELECT questions.*, (SELECT COUNT(*) FROM comments c WHERE c.question_id = questions.id AND c.archived = 0) AS total_question_comments
+    FROM questions
+    WHERE questions.archived = 0
+    ORDER BY total_question_comments ${order}
+    `;
   }
-});
 
-router.get("/:id", async (req, res) => {
   try {
     const con = await mysql.createConnection(dbConfig);
     const [data] = await con.execute(`
-    SELECT *
-    FROM questions
-    WHERE questions.id = 3
+    ${query}
     `);
     res.send(data);
     await con.end();
@@ -109,21 +111,5 @@ router.delete("/:id", isLoggedIn, async (req, res) => {
     res.status(400).send({ err: "Error Delete" });
   }
 });
-
-// router.delete("/:id", isLoggedIn, async (req, res) => {
-//   try {
-//     const con = await mysql.createConnection(dbConfig);
-//     const [data] = await con.execute(`
-//       DELETE FROM questions
-//       WHERE questions.user_id = ${req.params.user.id}
-//       AND questions.id = ${req.params.id}
-//     `);
-//     res.send(data);
-//     await con.end();
-//   } catch (err) {
-//     console.log(err);
-//     res.status(400).send({ err: "Error Delete" });
-//   }
-// });
 
 module.exports = router;
